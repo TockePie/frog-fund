57b2e41458a7dce4408cb08eab7cbb6420c6f66d
Maxim <144056146+TockePie@users.noreply.github.com>
2025-10-22 11:45:15 +0300

    routing base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,36 @@
-function App() {
+import { Link } from 'react-router'
+
+export default function App() {
   return (
-    <main className="flex items-center justify-center">
+    <main className="flex flex-col items-center justify-center gap-4">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <div className="flex flex-col items-center gap-4">
+        <span>
+          FrogFund покликаний спростити спосіб збору коштів та створенню
+          розіграшів.
+        </span>
+
+        <div className="flex gap-4">
+          <Link to="/campaigns">
+            <button
+              className="rounded-sm bg-stone-900 px-4 py-2 text-white hover:bg-stone-800 active:bg-stone-700"
+              type="button"
+            >
+              Мої збори
+            </button>
+          </Link>
+
+          <Link to="/raffles">
+            <button
+              className="rounded-sm bg-stone-900 px-4 py-2 text-white hover:bg-stone-800 active:bg-stone-700"
+              type="button"
+            >
+              Розіграші
+            </button>
+          </Link>
+        </div>
+      </div>
     </main>
   )
 }
-
-export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,21 @@
+// @ts-check
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
+import Campaigns from './pages/Campaigns/index.jsx'
 import App from './App.jsx'
 
 import './index.css'
 
+// @ts-ignore
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="campaigns" element={<Campaigns />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
