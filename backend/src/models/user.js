7d5aa627e20c3a0b3753d769532cad16e744cf29
Maxim <144056146+TockePie@users.noreply.github.com>
2025-11-10 12:44:35 +0200

    basic user type

---
+++ backend/src/models/user.js
@@ -0,0 +1,13 @@
+import * as z from 'zod'
+
+export const UserBodyObject = z.object({
+  name: z.string().min(2),
+  email: z.email(),
+  password: z.string().min(8, 'Password must be at least 8 characters.')
+})
+
+export const UserObject = UserBodyObject.extend({
+  id: z.uuidv4()
+}).omit({
+  password: true
+})
