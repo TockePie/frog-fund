829327d10401a022a464e55210948081d1b98ed5
Maxim <144056146+TockePie@users.noreply.github.com>
2025-11-18 15:57:58 +0200

    enhance page

--- frontend/src/pages/Campaign/NewCampaign/index.jsx
+++ frontend/src/pages/Campaign/NewCampaign/index.jsx
@@ -17,20 +17,20 @@ export default function CreateBank() {
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gradient-to-r from-[#ff7b7b] via-[#ff985f] to-[#ffd86f] p-6">
-      <div className="my-16 w-full max-w-xl rounded-3xl bg-stone-100 p-10 text-center shadow-xl">
-        <button
-          onClick={() => navigate(-1)}
-          className="absolute top-6 left-6 flex items-center gap-2 text-white transition hover:opacity-80"
-        >
-          <span className="text-2xl">←</span> Назад
-        </button>
+    <div className="relative flex items-center justify-center bg-gradient-to-r from-rose-400 via-orange-300 to-amber-300">
+      <button
+        onClick={() => navigate(-1)}
+        className="absolute top-6 left-6 flex items-center gap-2 text-white transition hover:opacity-80"
+      >
+        <span className="text-2xl">←</span> Назад
+      </button>
 
-        <h2 className="mb-8 text-3xl font-bold">Нова банка</h2>
+      <div className="my-20 flex w-full max-w-xl flex-col items-center gap-10 rounded-3xl bg-stone-100 p-10 px-5 text-center shadow-xl md:px-10">
+        <h2 className="text-3xl font-bold">Нова банка</h2>
 
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="mx-auto flex max-w-112 flex-col gap-6"
+          className="flex w-full max-w-112 flex-col gap-6"
         >
           <InputElement
             label="На що збираємо?"
