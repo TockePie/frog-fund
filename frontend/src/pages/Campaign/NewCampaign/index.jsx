adf24e05d9a0f19c83d0963e42652effb5abaf2a
Maxim <144056146+TockePie@users.noreply.github.com>
2025-11-20 14:04:55 +0200

    minor changes

--- frontend/src/pages/Campaign/NewCampaign/index.jsx
+++ frontend/src/pages/Campaign/NewCampaign/index.jsx
@@ -1,5 +1,8 @@
 import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router'
+import { ChevronLeft } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
 
 import InputElement from './input-element'
 import TextareaElement from './textarea-element'
@@ -17,60 +20,60 @@ export default function CreateBank() {
   }
 
   return (
-    <div className="relative flex items-center justify-center bg-gradient-to-r from-rose-400 via-orange-300 to-amber-300">
-      <button
+    <main className="relative flex items-center justify-center bg-gradient-to-r from-rose-400 via-orange-300 to-amber-300">
+      <Button
         onClick={() => navigate(-1)}
-        className="absolute top-6 left-6 flex items-center gap-2 text-white transition hover:opacity-80"
+        variant="ghost"
+        className="hover:text-primary absolute top-6 left-6 text-white transition hover:bg-white/60"
       >
-        <span className="text-2xl">←</span> Назад
-      </button>
+        <ChevronLeft className="size-6" /> Назад
+      </Button>
 
       <div className="my-20 flex w-full max-w-xl flex-col items-center gap-10 rounded-3xl bg-stone-100 p-10 px-5 text-center shadow-xl md:px-10">
         <h2 className="text-3xl font-bold">Нова банка</h2>
 
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex w-full max-w-112 flex-col gap-6"
+          className="flex w-full max-w-112 flex-col gap-10"
         >
-          <InputElement
-            label="На що збираємо?"
-            name="title"
-            control={control}
-            placeholder="На психолога"
-            rules={{ required: "Назва банки обов'язкова" }}
-          />
+          <div className="flex w-full flex-col gap-6">
+            <InputElement
+              label="На що збираємо?"
+              name="title"
+              control={control}
+              placeholder="На психолога"
+              rules={{ required: "Назва банки обов'язкова" }}
+            />
 
-          <TextareaElement
-            label="Опис"
-            name="description"
-            control={control}
-            placeholder="На психолога"
-          />
+            <TextareaElement
+              label="Опис"
+              name="description"
+              control={control}
+              placeholder="На психолога"
+            />
 
-          <InputElement
-            label="Сума накопичення"
-            name="target_amount"
-            control={control}
-            placeholder="0"
-            rightIcon={<p className="text-lg">₴</p>}
-          />
+            <InputElement
+              label="Сума накопичення"
+              name="target_amount"
+              control={control}
+              placeholder="0"
+              rightIcon={<p className="text-lg">₴</p>}
+            />
 
-          <InputElement
-            label="Мінімальний внесок"
-            name="min_amount"
-            control={control}
-            placeholder="0"
-            rightIcon={<p className="text-lg">₴</p>}
-          />
+            <InputElement
+              label="Мінімальний внесок"
+              name="min_amount"
+              control={control}
+              placeholder="0"
+              rightIcon={<p className="text-lg">₴</p>}
+            />
+          </div>
 
-          <button
-            type="submit"
-            className="mt-4 rounded-full bg-black py-3 text-lg font-bold text-white transition hover:opacity-80"
-          >
+          <Button type="submit" className="h-14">
             Створити
-          </button>
+          </Button>
         </form>
       </div>
-    </div>
+    </main>
   )
 }
